9e707644a818a5269267df312809bac4042e4502
Speaslee <sophiapeaslee@gmail.com>
2015-11-09 15:40:26 -0500

    commitment issues

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["836520278681-i6cc2drij901dlhrsnbkjqkke3actorj.apps.googleusercontent.com
-"], ENV["nqClMIiOofMA0n9K11VGotvs"],
+  provider :google_oauth2, ENV["836520278681-i6cc2drij901dlhrsnbkjqkke3actorj.apps.googleusercontent.com"], ENV["nqClMIiOofMA0n9K11VGotvs"],
   {name: 'google_login', approval_prompt:''}
 end

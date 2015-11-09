3edacd959c96ef53da4f948acd195d001bc5c878
Speaslee <sophiapeaslee@gmail.com>
2015-11-09 14:22:21 -0500

    fixed thing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["836520278681-nkql6d8efet8t10nnje57afqa6g9s1lg.apps.googleusercontent.com
-"], ENV["woIImbDGJiFZlc5EgCEV1Bzl"],
+  provider :google_oauth2, ENV["836520278681-i6cc2drij901dlhrsnbkjqkke3actorj.apps.googleusercontent.com
+"], ENV["nqClMIiOofMA0n9K11VGotvs"],
   {
     :name=>"google",
     :scope=>"email, profile, plus.me",

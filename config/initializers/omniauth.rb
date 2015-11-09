37da38604135469ba061b18da2486aac01410e7b
Speaslee <sophiapeaslee@gmail.com>
2015-11-09 15:33:40 -0500

    stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["836520278681-i6cc2drij901dlhrsnbkjqkke3actorj.apps.googleusercontent.com
 "], ENV["nqClMIiOofMA0n9K11VGotvs"],
-  {
-    :name=>"google",
-    :scope=>"email, profile, plus.me",
-    :prompt =>"select_acount"
-
-  }
+  {name: 'google_login', approval_prompt:''}
 end
